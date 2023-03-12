86b20fa684a106cf20fca2c33dbd01341a7e6a95
erick.traore <traoreerick@gmail.com>
2023-03-12 09:47:04 +0100

    page d/accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,43 @@
-    async function getPhotographers() {
+import Api from "../api/Api.js";
+
+
+
+async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+      
+        const photographersApi = await new Api('data/test-photographers-data.json')
+        const photographers = await photographersApi.get()
+        // let photographers = [
+        //     {
+        //         "name": "Ma data test",
+        //         "id": 1,
+        //         "city": "Paris",
+        //         "country": "France",
+        //         "tagline": "Ceci est ma data test",
+        //         "price": 400,
+        //         "portrait": "account.png"
+        //     },
+        //     {
+        //         "name": "Autre data test",
+        //         "id": 2,
+        //         "city": "Londres",
+        //         "country": "UK",
+        //         "tagline": "Ceci est ma data test 2",
+        //         "price": 500,
+        //         "portrait": "account.png"
+        //     },
+        // ]
+        // fetch('../../data/new-photographer-data.json')
+        // .then( res => res.json()) 
+        // .then( data => {
+        //     console.log(data)
+        // }) 
+
+        
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
     async function displayData(photographers) {
