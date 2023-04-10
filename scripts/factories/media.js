e16f051e544cb30f026d57999395ea3eb2870720
Traore <traoreerick@gmail.com>
2023-04-10 20:33:32 +0200

    echec count

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,5 +1,14 @@
+tableLikes = [];
 
-function mediaFactory(photographerName, image) {
+function mediaFactory(photographerName, data, tableMediasLength) {
+   const image = data.image;
+   console.log(data);
+   console.log(tableMediasLength);
+   let target = 0;
+ 
+ 
+
+  dataLikes = data.likes
    const  mediaName =    photographerName.replaceAll(' ', '-')
    const picture_1 = `assets/images/samplePhotos/${mediaName}/${image}`;
    const picture_2 = `assets/icons/vector.png`;
@@ -10,22 +19,41 @@ const type = getFileExtension(url[0])
 console.log(photographerName)
 console.log(mediaName)
 console.log(image)
-console.log(getFileExtension)
+newImage = image.replaceAll('_', ' ')
+console.log(newImage)
+const words = newImage.split(' ');
+console.log(words)
+const nb = words.length
+console.log(nb)
 
+function removeItem(arr, value) {
+    var i = 0;
+    while (i < arr.length) {
+      if (arr[i] === value) {
+        arr.splice(i, 1);
+      } else {
+        ++i;
+      }
+    }
+    return arr;
+  }
+  wordDeletet = words[nb - (1)];
 
-// console.log(image.src.split('.')[1])
+  nexArray = removeItem(words, wordDeletet);
 
-console.log(url)
-console.log(picture_1)
-
-function getUserMediaDOM(photographerName, image) {
+function getUserMediaDOM(photographerName, image, nbItem) {
+    console.log('id = ',nbItem);
+    const counter = dataLikes;
+    // let test = document.getElementsByClassName('.photographer__media__card__title__icone');
     const photographersMedia = document.querySelector(".photographer__media");
+
     var divPresent = document.createElement("div");
     divPresent.classList.add("photographer__media__card");
     photographersMedia.appendChild(divPresent);
 
     console.log(type)
     if(type === `.jpg`){
+      
     var img = document.createElement("img");
         img.setAttribute("src", picture_1)
         img.classList.add("photographer__media__card__img");
@@ -46,20 +74,46 @@ function getUserMediaDOM(photographerName, image) {
     var divTexte = document.createElement("div");
         divTexte.classList.add("photographer__media__card__title__texte");
         divTitle.appendChild(divTexte);
-        divTexte.innerHTML = url
+        divTexte.innerHTML = data.title
    
     var divOther = document.createElement("div");
         divOther.classList.add("photographer__media__card__title__icone");
         divTitle.appendChild(divOther);
+
     var divNumber = document.createElement("div");
-        divNumber.classList.add("photographer__media__card__title__number");
+        divNumber.classList.add("photographer__media__card__title__icone__number");
+        divNumber.setAttribute('id',nbItem)
         divOther.appendChild(divNumber);
+        divNumber.innerHTML = counter;
+
     var divLike = document.createElement("img");
         divLike.setAttribute("src", picture_2)
-        divLike.classList.add("photographer__media__card__title__heart");
+        divLike.classList.add("photographer__media__card__title__icone__heart");
+        divLike.setAttribute('id',nbItem)
         divLike.innerHTML = picture_2;
         divOther.appendChild(divLike);
+
+    // }
+      
     }
-return { picture_1, picture_2, image, type, getUserMediaDOM }
+    const totalLikes = document.querySelectorAll('.photographer__media__card__title__icone__number');
+    totalLikes.forEach((totalLike,) =>{
+      // e.preventDefault();
+      tableLikes.push(totalLike);
+    });
+    console.log(tableLikes);
+  const buttons = document.querySelectorAll('.photographer__media__card__title__icone__heart');
+   buttons.forEach(button => {
+  
+      button.addEventListener('click', function (e) {  
+        e.preventDefault();
+        target = +e.target.id;
+        console.log(target);
+
+      }, false);
+  })
+
+return { picture_1, picture_2, image, newImage, type, dataLikes, getUserMediaDOM }
 
  }
+ 
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,47 +1,85 @@
-import Api from "../api/Api.js";
-let photographerName = ''
-const id = parseInt(new URLSearchParams(window.location.search).get('id'))
-console.log(id)
- 
-        async function getPhotographers() {
-            const photographersApi = new Api('data/test-photographers.json')
-            const photographers = await photographersApi.get()
-        return ({
-            photographers })
-    }
+import Api from '../api/Api.js';
+const tableMedias = [];
+let photographerName = '';
 
-    async function displayData(photographers) {
-        console.log('pages:photographers',photographers);
-        photographers.photographers.forEach((photographer) => {
-        if (photographer.id === id) {
-            console.log('pages:photographer', photographer)
-            photographerName = photographer.name
-            const profilModel = profilFactory(photographer);
-            profilModel.getUserProfilDOM(photographer);
-        }
-        });
-        const dataMedias = photographers.medias;
-        const tableImages = [];
-        console.log('dataMedias', dataMedias)
-        dataMedias.forEach((dataMedia) => {
-        if (dataMedia.photographerId === id) {
-            console.log(dataMedia.photographerId)
-            console.log(dataMedia.image)
-            console.log(photographerName)
-            const mediaModel = mediaFactory(photographerName, dataMedia.image);
-            console.log(mediaModel);
-            mediaModel.getUserMediaDOM(photographerName, dataMedia.image);
-            // tableImages.push(dataMedia.image)
-        };
-        });
-      
-        }
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-        displayData(photographerName);
-        displayData(dataMedia.image);
-    };
+// Récupération dynamique de l'id de la page 
+const id = parseInt (new URLSearchParams (window.location.search).get ('id'));
+
+// création des données relative aux médias du photographe
+async function getData () {
+  const dataApi = new Api ('data/test-photographers.json');
+  const data = await dataApi.get ();
+  
+  data.photographers.forEach (dataPhotographer => {
     
-    init();
+    if (dataPhotographer.id === id) {
+      photographerName = dataPhotographer.name;
+      const profilModel = profilFactory(dataPhotographer);
+      profilModel.getUserProfilDOM(dataPhotographer);
+    }
+    
+  });
+
+
+  data.medias.forEach (dataMedia => {
+    if (dataMedia.photographerId === id) {
+      tableMedias.push (dataMedia);
+    }
+  });
+    
+  return {
+    photographerName, tableMedias
+    // data,
+  };
+}
+
+
+// Création de la fonction de trie
+function compareValues (key, order = 'asc') {
+  return function innerSort (a, b) {
+    if (!a.hasOwnProperty (key) || !b.hasOwnProperty (key)) {
+      // property doesn't exist on either object
+      return 0;
+    }
+    const varA = typeof a[key] === 'string' ? a[key].toUpperCase () : a[key];
+    const varB = typeof b[key] === 'string' ? b[key].toUpperCase () : b[key];
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return order === 'desc' ? comparison * -1 : comparison;
+  };
+}
+
+// Exécution de la fonction de trie avec récupération automatique du choix de la sélection .
+const select = document.querySelector ('select');
+select.addEventListener('change', (event) =>{
+  const value = event.target.value;
+  tableMedias.sort(compareValues(value));
+  console.log(tableMedias);
+  document.querySelector('.photographer__media').innerHTML =''
+  displayData();
+
+})
+
+// Afichage des données triées
+async function displayData() {
+  const tableMediasLength = tableMedias.length;
+  let y = 0;
+
+  // console.log(tableMediasLength);
+  tableMedias.forEach (tableMedia => {
+    let numItem = y++;
+    const mediaTitleModel = mediaFactory (photographerName, tableMedia, tableMediasLength);
+    mediaTitleModel.getUserMediaDOM (photographerName, tableMedia, numItem );
+  });
+
+}
+async function init () {
+  // Récupère les datas des photographes
+  const {data} = await getData ();
+  displayData (data);
+}
+init ();
